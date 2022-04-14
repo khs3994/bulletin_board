1f354db1511e4012c202f65bef8986c77c2c5ed1
김현승 <80810303+khs3994@users.noreply.github.com>
2022-04-14 23:52:15 +0900

    ✨ :: 뷰 구현

--- presentation/src/main/java/com/example/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/example/presentation/ui/splash/SplashActivity.kt
@@ -1,13 +1,25 @@
 package com.example.presentation.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
 import com.example.presentation.R
 import com.example.presentation.base.BaseActivity
 import com.example.presentation.databinding.ActivitySplashBinding
+import com.example.presentation.ui.account.AcountActivity
+import com.example.presentation.ui.main.MainActivity
+
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun init() {
+        loadingStart()
+    }
 
+    private fun loadingStart() {
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(applicationContext,AcountActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
