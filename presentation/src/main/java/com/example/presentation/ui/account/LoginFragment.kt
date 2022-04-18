1ea6495c3f83c1cbc9d32b0f6550173570abef33
김현승 <80810303+khs3994@users.noreply.github.com>
2022-04-18 20:50:48 +0900

    ✨ :: 뷰 구현

--- presentation/src/main/java/com/example/presentation/ui/account/LoginFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/account/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentLoginBinding
@@ -12,6 +13,10 @@ import com.example.presentation.databinding.FragmentLoginBinding
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
     override fun init() {
+        binding.fragment = this
+    }
 
+    fun goSignup(view: View){
+        this.findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
     }
 }
--- presentation/src/main/java/com/example/presentation/ui/account/SignupFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/account/SignupFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentSignupBinding
@@ -12,6 +13,10 @@ import com.example.presentation.databinding.FragmentSignupBinding
 
 class SignupFragment : BaseFragment<FragmentSignupBinding>(R.layout.fragment_signup) {
     override fun init() {
+        binding.fragment = this
+    }
 
+    fun goBack(view: View){
+        this.findNavController().popBackStack()
     }
 }
