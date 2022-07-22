931e3684121d17963588e23308a248e13c924261
김현승 <80810303+khs3994@users.noreply.github.com>
2022-07-22 20:00:37 +0900

    :sparkles: :: dataSourceModule 작성

---
+++ app/src/main/java/com/example/firbase_bulletin_board/di/module/DataSourceModule.kt
@@ -0,0 +1,26 @@
+package com.example.firbase_bulletin_board.di.module
+
+import com.example.data.remote.datasource.BoardDataSource
+import com.example.data.remote.datasource.datasourceimpl.BoardDataSourceImpl
+import com.example.data.remote.network.BoardAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataSourceModule {
+
+    @Provides
+    @Singleton
+    fun provideInfoDataSource (
+        boardAPI: BoardAPI,
+    ): BoardDataSource{
+        return BoardDataSourceImpl(
+            boardAPI
+        )
+    }
+
+}
--- app/src/main/java/com/example/firbase_bulletin_board/di/module/NetWorkModule.kt
+++ app/src/main/java/com/example/firbase_bulletin_board/di/module/NetWorkModule.kt
@@ -6,6 +6,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
